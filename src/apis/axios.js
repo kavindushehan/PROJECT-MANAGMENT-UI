9cd0c59e13c2739159fd85014de25a023c969e27
LahiruPrasad12 <lahirupr471@gmail.com>
2022-06-05 21:10:43 +0530

    resolved errors

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://reg14-new.herokuapp.co'
 })
 
 if (localStorage.getItem('JWT')) {
